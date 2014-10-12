a6f403bf289692520351c2386c03d34b1e305fe1
itotallyrock <itotallyrock@users.noreply.github.com>
2014-10-12 18:10:18 -0500

    Updated sections

--- js/contact_me.js
+++ js/contact_me.js
@@ -26,6 +26,7 @@ $(function() {
                     email: email,
                     message: message
                 },
+				dataType: "json",
                 cache: false,
                 success: function() {
                     // Success message
@@ -45,7 +46,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems there has been an error.  Please try later.  Sorry.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
