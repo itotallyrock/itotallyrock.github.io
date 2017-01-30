49e1a9502fa7a736f400e92e9ce209c16f838ecf
itotallyrock <itotallyrock@users.noreply.github.com>
2017-01-29 21:18:24 -0600

    Attempted to try to automate updating itemlist and fixed itemlist bug.

--- overwatch/checklist/js/app.js
+++ overwatch/checklist/js/app.js
@@ -34,34 +34,35 @@ let app = new Vue({
   methods: {
     fetchLists: function() {
       let self = this
-      if (localStorage.getItem('lootboxes') == null) {
-        let checklist = 'https://rawgit.com/itotallyrock/itotallyrock.github.io/master/overwatch/checklist/itemlist.json'
-        $.getJSON(checklist).then((lootboxes) => {
+      let checklist = 'https://rawgit.com/itotallyrock/itotallyrock.github.io/master/overwatch/checklist/itemlist.json'
+      $.getJSON(checklist).then((lootboxes) => {
+        if (localStorage.getItem('lootboxes') == null) {
           // TODO: Sort lootboxes put checked at the bottom of category and by decending value
           localStorage.setItem('lootboxes', JSON.stringify(lootboxes))
-
+        } else {
+          let stringified = JSON.stringify(lootboxes)
+          if (localStorage.getItem('lootboxes').replace('owned: true', 'owned: false') !== stringified) {
+            // File has difference (itemlist has been updated) so delete current
+            // TODO: Save current checklist to new list
+            localStorage.setItem('lootboxes', stringified)
+          }
+        }
+        try {
+          lootboxes = JSON.parse(localStorage.getItem('lootboxes'))
           // Set current page to latest lootbox
           let pages = Object.keys(lootboxes)
           self.currentPage = localStorage.getItem('previousPage') || pages[pages.length - 1]
           self.lootboxes = lootboxes
-        }).catch((xhrError) => {
+        } catch (jsonParseError) {
+          lootboxes = []
+          self.lootboxes = lootboxes
           modalApp.$set(modalApp, 'currentModal', error)
-          console.error(xhrError);
-        })
-      } else {
-        let lootboxes = {}
-        try {
-          lootboxes = JSON.parse(localStorage.getItem('lootboxes'))
-        } catch (e) {
-          console.error('Error Parsing JSON, resetting saved items', e);
-          localStorage.removeItem('lootboxes')
-          localStorage.removeItem('previousPage')
-          return modalApp.$set(modalApp, 'currentModal', 'error')
+          console.error(jsonParseError)
         }
-        let pages = Object.keys(lootboxes)
-        self.currentPage = localStorage.getItem('previousPage') || pages[pages.length - 1]
-        self.lootboxes = lootboxes
-      }
+      }).catch((xhrError) => {
+        modalApp.$set(modalApp, 'currentModal', error)
+        console.error(xhrError)
+      })
     }
   }
 })
